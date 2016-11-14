fd16d408f7b3970aab5a85bc21f5444fa743123e
Christian Uhlan <Christian@pub94-39.pub.ltu.se>
2016-11-14 11:32:42 +0100

    updateDatabase

--- www/Login.php
+++ www/Login.php
@@ -3,14 +3,13 @@
    env_variables:
    # Replace project, instance, database, user and password with the values obtained
    # when configuring your Cloud SQL instance.
-   MYSQL_DSN: mysql:unix_socket=/cloudsql/mysqldatabase;dbname=DATABASE
-   MYSQL_USER: root
-   MYSQL_PASSWORD: 'root'
+ 
    // Create connection
-   $conn = new mysqli(null, "root", 'root', "DATABASE", null, "/cloudsql/mysqldatabase"));
+   $conn = new mysqli(null, "root", 'root123', "DATABASE", null, "/cloudsql/mysql"));
 
    // Check connection
    if ($conn->connect_error) {
+       echo "Connected failed";
        die("Connection failed: " . $conn->connect_error);
    } 
    echo "Connected successfully";
