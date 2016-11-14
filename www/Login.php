098a47d79829fbeb0b053cb4252bc991c0617ba8
Christian Uhlan <Christian@pub94-39.pub.ltu.se>
2016-11-14 13:20:25 +0100

    updateDB

--- www/Login.php
+++ www/Login.php
@@ -1,16 +1,19 @@
 <?php
-   session_start();
-   env_variables:
-   # Replace project, instance, database, user and password with the values obtained
-   # when configuring your Cloud SQL instance.
- 
-   // Create connection
-   $conn = new mysqli(null, "root", 'root123', "DATABASE", null, "/cloudsql/mysql"));
 
-   // Check connection
-   if ($conn->connect_error) {
-       echo "Connected failed";
-       die("Connection failed: " . $conn->connect_error);
-   } 
-   echo "Connected successfully";
+
+
+
+   $app['database'] = function () use ($app) {
+    // Connect to CloudSQL from App Engine.
+    $dsn = getenv('MYSQL_DSN');
+    $user = getenv('MYSQL_USER');
+    $password = getenv('MYSQL_PASSWORD');
+    if (!isset($dsn, $user) || false === $password) {
+        throw new Exception('Set MYSQL_DSN, MYSQL_USER, and MYSQL_PASSWORD environment variables');
+    }
+
+    $db = new PDO($dsn, $user, $password);
+
+    return $db;
+};
 ?>
