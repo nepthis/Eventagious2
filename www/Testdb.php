f51e611c213c2d5e62ce93927e65225dd07a67f9
Christian Uhlan <Christian@pub92-138.pub.ltu.se>
2016-11-22 13:16:20 +0100

    sqlTest

--- www/Testdb.php
+++ www/Testdb.php
@@ -7,16 +7,18 @@
  echo "test av db innan";
  $db = new PDO($dsn,$user,$pass);
  echo "test av db";
- $res = ($db -> query("SELECT * FROM User"));
+ $res = ($db -> query("SELECT Name  FROM User WHERE UserID=1"));
  print_r($res);
 
-echo 'hej';
+ echo 'hej';
 
  foreach($res as $row) {
  	echo 'row';
  	print_r($row);
  }
-
  echo 'hopp';
+ 
+
+
 
  ?>
