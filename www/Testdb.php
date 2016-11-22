ebf99644635f35665eda6124f37636bd07f0c315
Christian Uhlan <Christian@pub92-138.pub.ltu.se>
2016-11-22 12:57:32 +0100

    sqlTest

--- www/Testdb.php
+++ www/Testdb.php
@@ -7,7 +7,16 @@
  echo "test av db innan";
  $db = new PDO($dsn,$user,$pass);
  echo "test av db";
- $res = $db -> query("SELECT 1337");
+ $res = ($db -> query("SELECT 1337"));
  print_r($res);
+
+echo 'hej';
+
+ for($res as $row) {
+ 	echo 'row';
+ 	print_r($row);
+ }
+
+ echo 'hopp';
 
  ?>
