b1ff2bb503b5f59b8406aa9ebf6e23b968db1c28
Christian Uhlan <Christian@pub92-206.pub.ltu.se>
2016-11-22 08:49:59 +0100

    sqlTest

--- www/login.php
+++ www/login.php
@@ -47,8 +47,8 @@
       Detta är ett test!
       <?php
         echo "Test ifrån php";
-       //$Test = $db -> select("SELECT * FROM User");
-       $Test = $db -> query(show databases);
+       $Test = $db -> select('SELECT 'Name' FROM 'User';');
+       //$Test = $db -> query("SELECT * FROM User");
        if ($Test === false){
         echo "Fel i select";
        }
