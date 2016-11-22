cc6894db3877b5a8a7f7ee4044436acd290b4041
Christian Uhlan <Christian@pub92-138.pub.ltu.se>
2016-11-22 12:44:02 +0100

    sqlTest

--- www/Testdb.php
+++ www/Testdb.php
@@ -1,8 +1,10 @@
 <?php
- $dsn = 'mysql:unix_socket=/cloudsql/eventagious3:us-central1:mysql;dbname=EventagiousProject'
- $user = 'root'
- $pass = ''
 
+ $dsn = 'mysql:unix_socket=/cloudsql/eventagious3:us-central1:mysql;dbname=EventagiousProject';
+ $user = 'root';
+ $pass = '';
+
+ echo "test av db innan";
  $db = new PDO($dsn,$user,$pass);
  echo "test av db";
  $res = $db -> query("SELECT 1337");
