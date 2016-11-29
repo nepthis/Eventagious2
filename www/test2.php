cabdf6c3ecc016f08418f5d00453f915cb99d28a
Christian Uhlan <Christian@pub92-118.pub.ltu.se>
2016-11-29 08:55:19 +0100

    API

--- www/test2.php
+++ www/test2.php
@@ -1,10 +1,10 @@
 <?php
 
 
-echo "Detta är i test filen!! för API";
+echo "Detta ar i test filen!! for API";
 
 
-/*
+
 $url = 'https://eventagious3.appspot.com/api/?user_id=1';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_HTTPGET, true);
@@ -13,7 +13,7 @@ $response_json = curl_exec($ch);
 curl_close($ch);
 $response=json_decode($response_json, true);
 echo $response;
-*/
+
 
 echo "Slutet på Filen";
 
