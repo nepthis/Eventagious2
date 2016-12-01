2874ddeff8106378898dcb364a65b48e58ddb497
Christian Uhlan <Christian@pub93-87.pub.ltu.se>
2016-12-01 10:47:49 +0100

    API

--- www/Templates/myPage.php
+++ www/Templates/myPage.php
@@ -9,15 +9,15 @@
       curl_close($ch);
       $response=json_decode($response_json, true);
 
-      print_r("<div class=""row"">");
+      echo("<div class=\"row\">");
      foreach($response as $row){
-        print_r("<div class=""col-md-4"">");
-        print_r("<h2>".$row['Eventname']"</h2>");
-        print_r("<p>".$row['Description']" </p>");
-        print_r("<p>".$row['Adress']" </p>");
-        print_r("<p>".$row['Section']" </p>");
-        print_r("<p><a class=""btn btn-default"" href=""index.php?action=eventInfo"" role=""button"">View details &raquo;</a></p>");
-        print_r("</div>");
+        echo("<div class=\"col-md-4\">");
+        echo("<h2>".$row['Eventname']"</h2>");
+        echo("<p>".$row['Description']"</p>");
+        echo("<p>".$row['Adress']"</p>");
+        echo("<p>".$row['Section']"</p>");
+        echo("<p><a class=\"btn btn-default\" href=\"index.php?action=eventInfo\" role=\"button\">View details &raquo;</a></p>");
+        echo("</div>");
       }
       ?>
 
