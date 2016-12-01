514bba589071b5fff4419da1f88a531690516de5
Christian Uhlan <Christian@pub93-87.pub.ltu.se>
2016-12-01 12:50:03 +0100

    API

---
+++ www/Templates/createevent.php
@@ -0,0 +1,32 @@
+    </div>
+   <div class="jumbotron">
+      <div class="container">
+        <h1>Skapa ett event</h1>
+        <p>Detta är en sida för att skapa event! </p>
+        <p>Man fyller i vad event ska heta, vad eventet ska handla om och vart det ska vara</p>
+      </div>
+    </div>
+
+
+
+    <div class="container">
+      <div class="input-group input-group-lg">
+        <span class="input-group-addon" id="sizing-addon1">@</span>
+        <input type="text" class="form-control" placeholder="Eventname" aria-describedby="sizing-addon1">
+      </div>
+      <p>Fylla i vad eventet ska heta</p>
+      <div class="input-group input-group-lg" style="padding-top: 5px">
+        <span class="input-group-addon" id="sizing-addon2">@</span>
+        <input type="text" class="form-control" placeholder="Description" aria-describedby="sizing-addon1">
+      </div>
+      <p>Fylla i vad eventet ska handla om</p>
+      <div class="input-group input-group-lg" style="padding-top: 5px">
+        <span class="input-group-addon" id="sizing-addon2">@</span>
+        <input type="text" class="form-control" placeholder="Adress" aria-describedby="sizing-addon1">
+      </div>
+      <p>Fylla i vart eventet ska vara </p>
+      <div class="input-group input-group-lg" style="padding-top: 5px">
+        <span class="input-group-addon" id="sizing-addon2">@</span>
+        <input type="text" class="form-control" placeholder="poss" aria-describedby="sizing-addon1">
+      </div>
+
