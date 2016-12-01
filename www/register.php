9f1008ae46ee54b9f1cd32802ee18ebec8ad55a4
Viktor Carlsson <Viktor Carlsson>
2016-12-01 10:20:05 +0100

    registertest

--- www/register.php
+++ www/register.php
@@ -31,13 +31,6 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  
-
-    <?php 
-    include_once "database.php";
-    include_once "PHPscript.php";
-    include_once "registerscript.php";
-    ?>
 
   </head>
 
