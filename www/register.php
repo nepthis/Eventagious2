5cebc4b11d52ea9b923b2a8afbb1c10f035dbbce
Viktor Carlsson <Viktor Carlsson>
2016-12-02 13:17:40 +0100

    registertest

--- www/register.php
+++ www/register.php
@@ -93,7 +93,7 @@
 <?php
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
   //$error_msg = "";
-  if(isset($_POST['username'], $_POST['email'], $_POST['password'], $_POST['firstname'], $_POST['surname'], $_POST['adress'], $_POST['section'])) {
+  //if(isset($_POST['username'], $_POST['email'], $_POST['password'], $_POST['firstname'], $_POST['surname'], $_POST['adress'], $_POST['section'])) {
     $username=$_REQUEST["username"];
     $email=$_REQUEST["email"];
     $password=$_REQUEST["password"];
@@ -121,9 +121,6 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $response_json = curl_exec($ch);
     curl_close($ch);
     $response=json_decode($response_json, true);
-  }
-  else {
-    echo "Fill all the fields."
-  }
 }
+//}
 ?>
