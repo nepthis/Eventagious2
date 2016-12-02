443978bde42aabc355fc370b69e0b7454e760425
Christian Uhlan <Christian@pub92-207.pub.ltu.se>
2016-12-02 13:46:02 +0100

    API

--- www/process_login.php
+++ www/process_login.php
@@ -6,26 +6,15 @@
 
       if (isset($_POST['username'], $_POST['password'])) {
       $username = $_POST['username'];
-<<<<<<< HEAD
-      $password = $_POST['password'];  // The not hashed password.
-=======
       $password = $_POST['password']; // The hashed password.
 
->>>>>>> c57103e6ab239c840011b84565a07be6c818dca6
         if (login($username, $password) == true) {
-<<<<<<< HEAD
             // Login success
           echo "Test av session";
           echo $_SESSION['username']; 
           //header('Location: https://eventagious3.appspot.com/index.php');
           //header('Location: http://localhost:8080/index.php');
           //exit;
-=======
-            // Login success 
-          header('Location: https://eventagious3.appspot.com/index.php');
-          //header('Location: http://localhost:8080/index.php');
-          exit;
->>>>>>> 2e10dc7abb8ae721a1ad1a171f00bba5890aa9a9
         } else {
           // Login failed
           echo "fel i inlog"; 
