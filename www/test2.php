632a010f9783b7b9240d71a3b73876522bec2e33
Christian Uhlan <Christian@pub92-62.pub.ltu.se>
2016-12-03 15:04:45 +0100

    CloudStorageTest

--- www/test2.php
+++ www/test2.php
@@ -1,10 +1,21 @@
 <?php
 
 
-echo "Detta ar i test filen!! for API";
+echo "Detta ar i test filen!! for CloudStorageTools";
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    echo "Uppladdningen funkade";
+}
 
 
+  use google\appengine\api\cloud_storage\CloudStorageTools;
+  $options = ['gs_bucket_name' => "eventagious3.appspot.com/img"];
 
+
+$upload_url = CloudStorageTools::createUploadUrl('/test2', $options);
+
+
+/*
 $url = 'https://eventagious3.appspot.com/api/?user_id_events=2';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_HTTPGET, true);
@@ -12,39 +23,13 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response_json = curl_exec($ch);
 curl_close($ch);
 $response=json_decode($response_json, true);
-
-/*
-foreach($response as $row) {
- 	print_r($row['Description']);
- }*/
-
-/*$length = sizeof($response);
-echo $length;
-      for ($i = 0; $i = $length; $i++) {
-        //print_r($response[i]['Eventname']);
-        print_r($response[i]['Description']);
-        print_r($response[i]['Adress']);
-        print_r($response[i]['Section']);
-}*/
-
-
-echo("<div class=\"row\">");
-      foreach($response as $row){
-        echo("<div class=\"col-md-4\">");
-        echo("<h2>".$row['Eventname']."</h2>");
-        echo("<p>".$row['Description']."</p>");
-        echo("<p>".$row['Adress']."</p>");
-        echo("<p>".$row['Section']."</p>");
-        echo("<p><a class=\"btn btn-default\" href=\"index.php?action=eventInfo\" role=\"button\">View details &raquo;</a></p>");
-        echo("</div>");
-	  }
-
-
-//echo "Testutskrifter";
-//Detta funkar fin fint.
-//print_r($response[0]['Description']);
-//print_r($response[1]['Description']);
+*/
 
 echo "Slutet på Filen";
 
 ?>
+<form action="{{ upload_url }}" enctype="multipart/form-data" method="post">
+    Files to upload: <br>
+   <input type="file" name="uploaded_files" size="40">
+   <input type="submit" value="Send">
+</form>
