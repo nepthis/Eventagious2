f012a28a1625538aa4af756b8b4e1745db3bb856
Christian Uhlan <Christian@pub94-4.pub.ltu.se>
2016-12-06 16:00:05 +0100

    CloudStorageAPI

--- www/api.php
+++ www/api.php
@@ -60,7 +60,7 @@ if ($path == '/help') {
       if(!empty($_GET["insert_img"]))
       {
           echo json_encode("går in i rätt");
-          insert_img();
+          //insert_img();
       }
       //Insert Event to event table
       if(!empty($_GET["event"]))
@@ -123,7 +123,7 @@ if ($path == '/help') {
   }
 
   function insert_img(){
-    require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
+    //require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
     use google\appengine\api\cloud_storage\CloudStorageTools;
  
     global $connection;
