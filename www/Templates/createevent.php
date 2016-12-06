740c9970ce1108410d8c858eb380a5de63ca3a62
Viktor Carlsson <Viktor Carlsson>
2016-12-06 14:59:00 +0100

    map key listener

--- www/Templates/createevent.php
+++ www/Templates/createevent.php
@@ -1,12 +1,5 @@
     </div>
-   <div class="jumbotron">
-      <div class="container">
-        <h1>Skapa ett event</h1>
-        <p>Detta är en sida för att skapa event! </p>
-        <p>Man fyller i vad event ska heta, vad eventet ska handla om och vart det ska vara</p>
-      </div>
-    </div>
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="../../dist/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
@@ -116,6 +109,14 @@
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCqlg8Lpg9t90hUKNPE_SPJLqgUfa27ETU&callback=initMap">
     </script>
+
+   <div class="jumbotron">
+      <div class="container">
+        <h1>Skapa ett event</h1>
+        <p>Detta är en sida för att skapa event! </p>
+        <p>Man fyller i vad event ska heta, vad eventet ska handla om och vart det ska vara</p>
+      </div>
+    </div>
 
 
   <div class="container">
