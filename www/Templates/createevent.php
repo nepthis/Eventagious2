66037385892a13f99a36f192caae2ae3266bdf53
Viktor Carlsson <Viktor Carlsson>
2016-12-06 16:11:46 +0100

    map key listener

--- www/Templates/createevent.php
+++ www/Templates/createevent.php
@@ -1,5 +1,5 @@
-    </div>
-         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="../../dist/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
@@ -109,7 +109,7 @@
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCqlg8Lpg9t90hUKNPE_SPJLqgUfa27ETU&callback=initMap">
     </script>
-
+    </div>
    <div class="jumbotron">
       <div class="container">
         <h1>Skapa ett event</h1>
