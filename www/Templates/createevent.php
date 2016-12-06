4b34225b043ef149335645740dc565ce80fd4443
Viktor Carlsson <Viktor Carlsson>
2016-12-06 16:26:27 +0100

    map key listener

--- www/Templates/createevent.php
+++ www/Templates/createevent.php
@@ -1,4 +1,11 @@
-</div>
+      <div class="container">
+          <div id="map" style="height: 500px; width:1200px; text-align: center; "></div>
+      </div>
+
+
+
+      <div class="container">
+      </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="../../dist/js/bootstrap.min.js"></script>
