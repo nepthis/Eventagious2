97b83a0d87c68f6a5b1929db3ae95793c5ace5c1
Viktor Carlsson <Viktor Carlsson>
2016-12-07 15:44:48 +0100

    map key listener

---
+++ www/Templates/admin.php
@@ -0,0 +1,11 @@
+    <div class="jumbotron">
+      <div class="container">
+        <h1>ADMINSIDA</h1>
+        <p>HEJ ADMIN DU ÄR SÖT </p>
+        <p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a>
+        <a class="btn btn-primary btn-lg" href="index.php?action=map" role="button">Map &raquo;</a>
+        <a class="btn btn-primary btn-lg" href="index.php?action=index" role="button">Register &raquo;</a>
+        </p>
+      </div>
+    </div>
