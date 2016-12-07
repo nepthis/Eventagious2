a4f02ca9b46ff18b5a259a941b9a2e88d7f7d0aa
Christian Uhlan <Christian@pub93-65.pub.ltu.se>
2016-12-07 14:09:42 +0100

    img

--- www/test2.php
+++ www/test2.php
@@ -15,45 +15,19 @@ $response=json_decode($response_json, true);
 
 
 <html>
+<head>
+<script type="text/javascript" src="js/jquery-1.11.3-jquery.min.js"></script>
+<script type="text/javascript" src="assets/js/upload_js.js"></script>
+
+</head>
+
 <body>
-<form action="https://eventagious3.appspot.com/api/?insertImg=1" method="post" enctype="multipart/form-data">
+<form action="https://eventagious3.appspot.com/api/?insertImg=1" method="post" enctype="multipart/form-data" id="form" >
   Send these files:<p/>
-  <input name="file" type="file" multiple="multiple"/><p/>
+  <input name="file" type="file" id ="file" multiple="multiple"/><p/>
   <input name="EventID" type="text" id="EventID" placeholder="EventID" value="12"/><p/>
   <input name="FileName" type="text" id="FileName" placeholder="FileName" value="file"/><p/>
-  <input type="submit" value="Send files" />
+  <input id= button type="submit" value="Send files" />
 </form>
 </body>
 </html>
-
-<script type="text/javascript">
-	$(document).ready(function (e) {
-    $('#imageUploadForm').on('submit',(function(e) {
-        e.preventDefault();
-        var formData = new FormData(this);
-
-        $.ajax({
-            type:'POST',
-            url: $(this).attr('action'),
-            data:formData,
-            cache:false,
-            contentType: false,
-            processData: false,
-            success:function(data){
-                console.log("success");
-                console.log(data);
-            },
-            error: function(data){
-                console.log("error");
-                console.log(data);
-            }
-        });
-    }));
-
-    $("#ImageBrowse").on("change", function() {
-        $("#imageUploadForm").submit();
-    });
-});
-
-	
-</script>
