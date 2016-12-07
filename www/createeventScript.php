cf78c740791b22ee124daf1d9ce3f1c4a5919573
Viktor Carlsson <Viktor Carlsson>
2016-12-07 14:56:41 +0100

    map key listener

--- www/createeventScript.php
+++ www/createeventScript.php
@@ -23,6 +23,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     'Eventname' => $Eventname,
     'EventDate' => $EventDate,
     );
+    echo ($EventDate);
     $url = 'https://eventagious3.appspot.com/api/?event=1';
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_POST, true);
