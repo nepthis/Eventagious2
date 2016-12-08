2bc0f8844e455a3638b521831f89af7b79d190a9
Christian Uhlan <Christian@pub94-27.pub.ltu.se>
2016-12-08 14:16:52 +0100

    imgUpdate

--- www/Templates/admin.php
+++ www/Templates/admin.php
@@ -6,36 +6,37 @@
     </div>
 
 
-      <div class="table-responsive">
-          <table class="table table-hover">
-            <thead>
-            <tr>
-              <th>Firstname</th>
-              <th>Lastname</th>
-              <th>Email</th>
-            </tr>
-          </thead>
-          <tbody>
-              <tr>
-                <td>John</td>
-                <td>Doe</td>
-                <td>john@example.com</td>
-              </tr>
-              <tr>
-                <td>Mary</td>
-                <td>Moe</td>
-                <td>mary@example.com</td>
-              </tr>
-              <tr>
-                <td>July</td>
-                <td>Dooley</td>
-                <td>july@example.com</td>
-              </tr>
-            </tbody>
-          </table>
-    </div>
+      
 <?php
-  
+    $url = 'https://eventagious3.appspot.com/api/?get_all_events=1';
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_HTTPGET, true);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      $response_json = curl_exec($ch);
+      curl_close($ch);
+      $responseEvent=json_decode($response_json, true);
+
+    echo ("<div class=\"table-responsive\">");
+          echo ("<table class=\"table table-hover\">");
+            echo ("<thead>");
+            echo ("<tr>");
+              echo ("<th>Event Name</th>");
+              echo ("<th>UserID</th>");
+              echo ("<th>Lastname</th>");
+
+            echo ("</tr>");
+          echo ("</thead>");
+          echo ("<tbody>");
+            foreach($responseEvent as $rowEvent){
+              echo ("<tr>");
+                echo ("<td>".$rowEvent['Eventname']."</td>");
+                echo ("<td>".$rowEvent['UserID']."</td>");
+                echo ("<td>".$rowEvent['Eventname']."</td>");
+              echo ("</tr>");
+            }
+            echo ("</tbody>");
+          echo ("</table>");
+    echo ("</div>");
 
 
 
