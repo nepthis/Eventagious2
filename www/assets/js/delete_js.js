76901c72f7766a03c1db6b3bc903b18269ec1621
Christian Uhlan <Christian@pub93-70.pub.ltu.se>
2016-12-09 09:43:09 +0100

    deleteUser

--- www/assets/js/delete_js.js
+++ www/assets/js/delete_js.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
   e.preventDefault();
 
   var id = $(this).data("id");
-
+  var url;
   if ($(e.target).data('action') === 'delete_event_id'){
-            var url = "https://eventagious3.appspot.com/api/?delete_event_id=" + id;
+            url = "https://eventagious3.appspot.com/api/?delete_event_id=" + id;
          }
-  if ($(e.target).data('action') === 'delete_user_id'){
-            var url = "https://eventagious3.appspot.com/api/?delete_user_id=" + id;
+  else if ($(e.target).data('action') === 'delete_user_id'){
+            url = "https://eventagious3.appspot.com/api/?delete_user_id=" + id;
          }
 
 
