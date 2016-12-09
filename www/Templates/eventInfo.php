ebd40cd569a1bdedba6885cecaa663cfb1107995
Christian Uhlan <Christian@pub93-70.pub.ltu.se>
2016-12-09 15:38:31 +0100

    eventNameInform

--- www/Templates/eventInfo.php
+++ www/Templates/eventInfo.php
@@ -1,14 +1,75 @@
 <script type="text/javascript" src="assets/js/upload_js.js"></script>
-<div class="container">
-          <div id="map" style="height: 500px; width:1200px; text-align: center; padding-top: 0px;"></div>
-      </div>
+<?php
+session_start();
+
+		  $EventID = $_GET['EventID'];
+	    $user_id = $_SESSION['user_id'];
+
+      echo $user_id;
+
+	    $url = 'https://eventagious3.appspot.com/api/?event_id='.$EventID.'';
+	    $ch = curl_init($url);
+	    curl_setopt($ch, CURLOPT_HTTPGET, true);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    $response_json = curl_exec($ch);
+	    curl_close($ch);
+	    $response=json_decode($response_json, true);
 
 
+      echo ("<div class=\"container\">");
+           echo ("<div id=\"map\" style=\"height: 500px; width:1200px; text-align: center; padding-top: 0px;\"></div>");
+       echo ("</div>");
 
-      <div class="container">
-      </div>
-    
-    </div>
+
+	    foreach($response as $row){
+	      	echo ("<div class=\"jumbotron\">");
+      		echo ("<div class=\"container\">");
+	        	//echo ("<div id=\"map\" style=\"height: 500px; width:1200px; text-align: center;\"></div>");
+	        	echo ("<h1>".$row['Eventname']."</h1>");
+	        	echo ("<p>Detta är en korkad test som ingen vad vad vi ska ha den till men va faN!. Heja TRUMP!.</p>");
+      			echo ("</div>");
+    		echo ("</div>");
+    		echo ("</div>");
+    		echo ("<div class=\"container\">");
+    			echo("<div class=\"row\">");
+	        		echo("<div class=\"col-lg-3\">");
+	        			//echo("<h2>".$row['Eventname']."</h2>");
+				        echo("<p>".$row['Description']."</p>");
+				        echo("<p>".$row['Adress']."</p>");
+				        echo("<p>".$row['EventDate']."</p>");
+				        echo("<p>".$row['Section']."</p>");
+	        		echo("</div>");
+	        	
+	    }
+	    		
+	    //Måste fixa så att den hämtar rätt ifrån APIn och så att den läggaer till rätt...
+
+	   	$url = 'https://eventagious3.appspot.com/api/?eventImg='.$EventID.'';
+	    $ch = curl_init($url);
+	    curl_setopt($ch, CURLOPT_HTTPGET, true);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    $response_json = curl_exec($ch);
+	    curl_close($ch);
+	    $response=json_decode($response_json, true);
+
+
+	    foreach($response as $row){
+			
+	     	echo ("<img src=".$row['Image_Thumbnail_URL'].">");
+		}
+				echo("</div>");
+		echo("</div>");
+		echo ("<div class=\"container\">");
+			echo ("<form method=\"post\" enctype=\"multipart/form-data\" id=\"form\" >");
+	  		echo ("Send these files:<p/>");
+	  		echo ("<input name=\"file\" type=\"file\" id =\"file\" multiple=\"multiple\"/><p/>");
+	  		echo ("<input name=\"EventID\" type=\"text\" id=\"EventID\" placeholder=\"EventID\" value=".$EventID."><p/>");
+	  		echo ("<input name=\"FileName\" type=\"text\" id=\"FileName\" placeholder=\"FileName\" value=\"file\"/><p/>");
+	  		echo ("<input id= \"button\" type=\"submit\" value=\"Upload\" />");
+		echo ("</div>");
+
+?>
+
     <script>
       // Note: This example requires that you consent to location sharing when
       // prompted by your browser. If you see the error "The Geolocation service
@@ -84,63 +145,3 @@
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCqlg8Lpg9t90hUKNPE_SPJLqgUfa27ETU&callback=initMap">
     </script>
-<?php
-
-		$EventID = $_GET['EventID'];
-	    $user_id = $_SESSION['user_id'];
-
-	    $url = 'https://eventagious3.appspot.com/api/?event_id='.$EventID.'';
-	    $ch = curl_init($url);
-	    curl_setopt($ch, CURLOPT_HTTPGET, true);
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	    $response_json = curl_exec($ch);
-	    curl_close($ch);
-	    $response=json_decode($response_json, true);
-
-
-	    foreach($response as $row){
-	      	echo ("<div class=\"jumbotron\">");
-      		echo ("<div class=\"container\">");
-	        	//echo ("<div id=\"map\" style=\"height: 500px; width:1200px; text-align: center;\"></div>");
-	        	echo ("<h1>".$row['Eventname']."</h1>");
-	        	echo ("<p>Detta är en korkad test som ingen vad vad vi ska ha den till men va faN!. Heja TRUMP!.</p>");
-      			echo ("</div>");
-    		echo ("</div>");
-    		echo ("</div>");
-    		echo ("<div class=\"container\">");
-    			echo("<div class=\"row\">");
-	        		echo("<div class=\"col-lg-3\">");
-	        			//echo("<h2>".$row['Eventname']."</h2>");
-				        echo("<p>".$row['Description']."</p>");
-				        echo("<p>".$row['Adress']."</p>");
-				        echo("<p>".$row['EventDate']."</p>");
-				        echo("<p>".$row['Section']."</p>");
-	        		echo("</div>");
-	        	
-	    }
-	    		
-	    //Måste fixa så att den hämtar rätt ifrån APIn och så att den läggaer till rätt...
-
-	   	$url = 'https://eventagious3.appspot.com/api/?eventImg='.$EventID.'';
-	    $ch = curl_init($url);
-	    curl_setopt($ch, CURLOPT_HTTPGET, true);
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	    $response_json = curl_exec($ch);
-	    curl_close($ch);
-	    $response=json_decode($response_json, true);
-
-
-	    foreach($response as $row){
-			
-	     	echo ("<img src=".$row['Image_Thumbnail_URL'].">");
-		}
-				echo("</div>");
-		echo("</div>");
-		echo ("<div class=\"container\">");
-			echo ("<form method=\"post\" enctype=\"multipart/form-data\" id=\"form\" >");
-	  		echo ("Send these files:<p/>");
-	  		echo ("<input name=\"file\" type=\"file\" id =\"file\" multiple=\"multiple\"/><p/>");
-	  		echo ("<input name=\"EventID\" type=\"text\" id=\"EventID\" placeholder=\"EventID\" value=".$EventID."><p/>");
-	  		echo ("<input name=\"FileName\" type=\"text\" id=\"FileName\" placeholder=\"FileName\" value=\"file\"/><p/>");
-	  		echo ("<input id= \"button\" type=\"submit\" value=\"Upload\" />");
-		echo ("</div>");
