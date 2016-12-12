ddaa5fbac6b0e69653bd5215c5053795b57087e6
Viktor Carlsson <Viktor Carlsson>
2016-12-12 13:23:21 +0100

    eventinfokarta

--- www/Templates/map.php
+++ www/Templates/map.php
@@ -15,28 +15,7 @@
     </div>
 
     <div class="container">
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-md-4">
-          <h2>Event 1</h2>
-          <p>Här kommer då De eventen som man ska gå på eller som man själva har skapat. </p>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. </p>
-          <p><a class="btn btn-default" href="index.php?action=eventInfo" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-          <h2>Event 2</h2>
-          <p>Här kommer då De eventen som man ska gå på eller som man själva har skapat. </p>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. </p>
-          <p><a class="btn btn-default" href="index.php?action=eventInfo role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-4">
-          <h2>Event 3</h2>
-          <p>Här kommer då De eventen som man ska gå på eller som man själva har skapat. </p>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. </p>
-          <p><a class="btn btn-default" href="index.php?action=eventInfo role="button">View details &raquo;</a></p>
-        </div>
-      </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="../../dist/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
