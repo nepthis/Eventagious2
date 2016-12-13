2563a454e35cf6a930b3450d7cf4f471baa03b88
Viktor Carlsson <Viktor Carlsson>
2016-12-13 11:25:33 +0100

    eventdescription

--- www/Templates/eventInfo.php
+++ www/Templates/eventInfo.php
@@ -21,7 +21,7 @@
 
 	    foreach($response as $row){
 	      	echo ("<div class=\"jumbotron\">");
-      		echo ("<div class=\"container\" style=\"width:1200px; text-align: center; padding-top: 0px;\">");
+      		echo ("<div class=\"container\">");
 	        	//echo ("<div id=\"map\" style=\"height: 500px; width:1200px; text-align: center;\"></div>");
 	        	echo ("<h1>".$row['Eventname']."</h1>");
             echo ("<h4>".$row['Description']."<h4>");
