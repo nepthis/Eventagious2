cc7b88f2706974a9f2321790aaf67a6bec6ed2b6
Viktor Carlsson <Viktor Carlsson>
2016-12-13 10:00:59 +0100

    search

--- www/Templates/searchevent.php
+++ www/Templates/searchevent.php
@@ -3,8 +3,8 @@
       //$user_id = $_SESSION['user_id'];
       //echo $user_id;
 
-//if($_SERVER['REQUEST_METHOD'] === 'GET'){
-//  $srch=$_GET["srch-term"];
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+  $srch=$_POST["srch-term"];
 //}
 
 
@@ -12,7 +12,7 @@
 
 
 
-      $url = 'https://eventagious3.appspot.com/api/?search_event=data';//+$srch;
+      $url = 'https://eventagious3.appspot.com/api/?search_event='+$srch;
       $ch = curl_init($url);
       curl_setopt($ch, CURLOPT_HTTPGET, true);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
