854fcbe48ba79143b24e497ba81747ef27c5e6b1
Christian Uhlan <Christian@pub92-164.pub.ltu.se>
2017-01-25 11:57:01 +0100

    update

--- www/Templates/map.php
+++ www/Templates/map.php
@@ -17,7 +17,7 @@
     <div class="container">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
+
 
 
 
